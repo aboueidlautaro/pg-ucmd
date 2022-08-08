151fe98d3aa12e1308c96712212edbaeaff22121
Lautaro Abou-Eid <70718664+aboueidlautaro@users.noreply.github.com>
2022-08-08 05:50:10 -0300

    Update InstagramFeed.jsx

--- src/assets/components/InstagramFeed.jsx
+++ src/assets/components/InstagramFeed.jsx
@@ -11,21 +11,7 @@ function InstagramFeed() {
   //});
   //}, []);
 
-  return (
-    <>
-      <Carousel cols={1} rows={1} gap={10} loop>
-        <Carousel.Item>
-          <img width="100%" src="https://picsum.photos/800/600?random=1" />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img width="100%" src="https://picsum.photos/800/600?random=2" />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img width="100%" src="https://picsum.photos/800/600?random=3" />
-        </Carousel.Item>
-      </Carousel>
-    </>
-  );
+  return <></>;
 }
 
 export default InstagramFeed;
