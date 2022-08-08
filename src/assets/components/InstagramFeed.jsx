2678c9a225490b89dab426520ec6372f4e550320
Lautaro Abou-Eid <70718664+aboueidlautaro@users.noreply.github.com>
2022-08-08 05:47:23 -0300

    test bug

--- src/assets/components/InstagramFeed.jsx
+++ src/assets/components/InstagramFeed.jsx
@@ -13,30 +13,17 @@ function InstagramFeed() {
 
   return (
     <>
-      <div className="my-8 py-6 rounded-lg bg-rosa-pastel shadow-md shadow-grafito-claro/60">
-        <Carousel cols={1} rows={1} gap={10} loop>
-          {jeison.data.map((value, index) => {
-            return (
-              <Carousel.Item key={index}>
-                <div className="flex justify-center items-center">
-                  <a
-                    href={value.permalink}
-                    className="h-[450px]  sm:h-96 w-full flex sm:flex-row flex-col justify-start sm:justify-center items-center"
-                  >
-                    <img
-                      className="shadow-md shadow-grafito-claro/60 rounded-md max-w-72 max-h-72 sm:max-w-96 sm:w-96 sm:h-full sm:max-h-full mx-2"
-                      src={value.media_url}
-                    />
-                    <p className="text-[#303030] font-medium mx-2 max-h-1/3 sm:max-h-96 w-full inline-block text-ellipsis">
-                      {value.caption}
-                    </p>
-                  </a>
-                </div>
-              </Carousel.Item>
-            );
-          })}
-        </Carousel>
-      </div>
+      <Carousel cols={1} rows={1} gap={10} loop>
+        <Carousel.Item>
+          <img width="100%" src="https://picsum.photos/800/600?random=1" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img width="100%" src="https://picsum.photos/800/600?random=2" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img width="100%" src="https://picsum.photos/800/600?random=3" />
+        </Carousel.Item>
+      </Carousel>
     </>
   );
 }
